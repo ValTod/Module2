99fa45a8abb561f8f6ca8fb9d80448278fee1132
ValTod <valtodus@gmail.com>
2024-01-16 11:17:43 +0200

    Added C# code MethodsLecture

---
+++ MethodsLecture/PoisitiveNegativeNull/PositiveNegative.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PoisitiveNegativeNull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            string word = Sign(x);
+            PrintSign(word, x);
+
+        }
+        static void PrintSign(string s, int number)
+        {
+            Console.WriteLine($"the sign of the number {number} is: {s}");
+        }
+
+        static string Sign(int number)
+        {
+            string x = "";
+            if (number > 0)
+                x= "+";
+            else if (number < 0)
+                x= "-";
+            else
+                x="0";
+            return x;
+        }
+
+    }
+}
---
+++ MethodsLecture/PrintDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PrintDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Receipt:");
+            PrintHeader();
+            PrintValues(x);
+            PrintArrows();
+            double d = 348*PrintDoubles(x);
+            PrintHeader();
+            PrintValues((int)d);
+
+        }
+
+        static void PrintHeader()
+        {
+            Console.WriteLine("----------");
+        }
+
+        static void PrintArrows ()
+        {
+            Console.WriteLine("-> -> -> ->");
+        }
+        static void PrintValues(int n)
+        {
+            Console.WriteLine($"the value is: {n}");
+        }
+        static double PrintDoubles(int n) => n / 125;
+        //{
+        //    double d = n / 125;
+        //    return d;
+        //}        
+    }
+}
